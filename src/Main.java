96b93981c308917dcc5a3f03046c86b6616eef59
Dennshirennshij <111683708+Dennshirennshij@users.noreply.github.com>
2024-02-14 11:11:24 +0100

    Added Trees class

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 import trees.Tree;
+import trees.Trees;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Tree<Integer> tree = new Tree<>() {
-            protected boolean compare(Integer a, Integer b) {
-                return a <= b;
-            }
-        };
+        Tree<Integer> tree = new Trees.IntTree();
         ArrayList<String> list = tree.inOrderString();
         for (String str : list) {
             System.out.println(str);
